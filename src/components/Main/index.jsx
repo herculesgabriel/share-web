af0b3371c6713efb8114e4750db78e417c77d761
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-09 12:37:39 -0300

    create homepage with the basic struture

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,44 @@
+import './style.css';
+
+const Main = () => (
+  <main className="messages">
+    <section>
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque, libero et convallis maximus, sapien lacus dapibus urna, at rutrum diam augue at turpis. Mauris et magna nisl.
+        </p>
+        <strong>MARIA DA SILVA - 09/01/2021</strong>
+      </article>
+
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque, libero et convallis maximus, sapien lacus dapibus urna, at rutrum diam augue at turpis. Mauris et magna nisl.
+        </p>
+        <strong>MARIA DA SILVA - 09/01/2021</strong>
+      </article>
+
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque, libero et convallis maximus, sapien lacus dapibus urna, at rutrum diam augue at turpis. Mauris et magna nisl.
+        </p>
+        <strong>MARIA DA SILVA - 09/01/2021</strong>
+      </article>
+
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque, libero et convallis maximus, sapien lacus dapibus urna, at rutrum diam augue at turpis. Mauris et magna nisl.
+        </p>
+        <strong>MARIA DA SILVA - 09/01/2021</strong>
+      </article>
+    </section>
+    <button className="load-messages">
+      Carregar mais mensagens
+    </button>
+
+    <footer>
+      <p>Created by um monte de gente bla bla bla...</p>
+    </footer>
+  </main>
+);
+
+export default Main;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+import Main from '../../components/Main';
+
+const Home = () => (
+  <>
+    <Header />
+    <Main />
+  </>
+);
+
+export default Home;
